f659c637caeb70a50ab34fb4273884ef2bc8fb5f
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2021-10-08 23:40:07 +0200

    correct domain coordinates

--- reference/parse_pfam_domains.py
+++ reference/parse_pfam_domains.py
@@ -1,7 +1,7 @@
 import json
 
 
-data = json.load(open("./reference/sars_cov_2.json"))
+data = json.load(open("./sars_cov_2.json"))
 
 pfam_domains = []
 for g in data["genes"]:
@@ -10,18 +10,22 @@ for g in data["genes"]:
             for p in t2["protein_features"]:
                 if p["dbname"] == "Pfam":
                     pfam_domains.append({
-                        "start": int(g["start"]) + int(p["start"]) - 1,     # from 1-based to 0-based
-                        "end": int(g["start"]) + int(p["end"]) - 1,
+                        # domains coordinates are in the protein space
+                        # from 1-based to 0-based
+                        "start": int(g["start"]) + (int(p["start"]) * 3) - 1,
+                        "end": int(g["start"]) + (int(p["end"]) * 3) - 1,
                         "interpro_name":p["interpro_name"],
                         "interpro_description": p["interpro_description"]
                     })
 
 chromosome = "MN908947.3"
 
-with open("reference/pfam_names.bed", "w") as f:
+pfam_domains = sorted(pfam_domains, key=lambda x: x["start"])
+
+with open("pfam_names.bed", "w") as f:
     for d in pfam_domains:
         f.write("\t".join([chromosome, str(d["start"]), str(d["end"]), d["interpro_name"]]) + "\n")
 
-with open("reference/pfam_descriptions.bed", "w") as f:
+with open("pfam_descriptions.bed", "w") as f:
     for d in pfam_domains:
         f.write("\t".join([chromosome, str(d["start"]), str(d["end"]), d["interpro_description"]]) + "\n")
