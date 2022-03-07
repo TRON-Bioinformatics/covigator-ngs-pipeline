e27eca237cb562a2ecc75224ea21b46900c5da0b
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2022-03-07 16:25:34 +0100

    add automated tests for phasing

--- tests/test_09.sh
+++ tests/test_09.sh
@@ -9,4 +9,5 @@ source venv/bin/activate
 pip install -r bin/requirements.txt
 python3 -m unittest bin/test_assembly_variant_caller.py
 python3 -m unittest bin/test_reference_genome.py
+python3 -m unittest bin/test_phasing.py
 deactivate
