d41a5f082482221bdcbde7b22e66d0425bfe067f
Jo Hausmann <35309108+johausmann@users.noreply.github.com>
2023-10-10 09:49:55 +0200

    Update test_16.sh
    Make CodeFactor happy

--- tests/scripts/test_16.sh
+++ tests/scripts/test_16.sh
@@ -13,4 +13,4 @@ nextflow main.nf -profile conda --name test_data \
     --lineage_mode
 
 test -s $output/test_data.assembly.pangolin.csv || { echo "Missing pangolin output file (lineage mode with vcf input)!"; exit 1; }
-assert_eq `wc -l $output/test_data.assembly.pangolin.csv` 2 "Wrong number of pangolin results"
+assert_eq $(wc -l $output/test_data.assembly.pangolin.csv) 2 "Wrong number of pangolin results"
