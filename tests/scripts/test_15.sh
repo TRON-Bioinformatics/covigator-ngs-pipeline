ea09b0b89bd1b19ec45a17868c73ef7c48b6f6ce
Jo Hausmann <35309108+johausmann@users.noreply.github.com>
2023-10-10 09:49:22 +0200

    Update test_15.sh
    Make CodeFactor happy

--- tests/scripts/test_15.sh
+++ tests/scripts/test_15.sh
@@ -14,7 +14,7 @@ nextflow main.nf -profile conda --name test_data \
 
 test -s $output/test_data.input.fasta || { echo "Missing VCF2FASTA fasta file (lineage mode with vcf input)!"; exit 1; }
 test -s $output/test_data.input.pangolin.csv || { echo "Missing pangolin output file (lineage mode with vcf input)!"; exit 1; }
-assert_eq `wc -l $output/test_data.input.pangolin.csv` 2 "Wrong number of pangolin results"
+assert_eq $(wc -l $output/test_data.input.pangolin.csv) 2 "Wrong number of pangolin results"
 
 
 
