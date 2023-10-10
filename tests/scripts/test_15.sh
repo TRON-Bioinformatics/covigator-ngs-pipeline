16011f7ff32bda672de36c1c4df5ca31c34a3051
Johannes Hausmann <johannes.hausmann@posteo.de>
2023-10-10 11:36:47 +0200

    Last push did not include updated test scripts

--- tests/scripts/test_15.sh
+++ tests/scripts/test_15.sh
@@ -9,12 +9,13 @@ source bin/assert.sh
 output=tests/output/test15
 nextflow main.nf -profile conda --name test_data \
 	--output $output \
-	--vcf $(pwd)/tests/test_data/test_data.lofreq.vcf \
-    --lineage_mode
+	--vcf $(pwd)/tests/test_data/test_data.assembly.lineage_mode.vcf.gz \
+        --lineage_mode \
+        --skip_normalization
 
 test -s $output/test_data.input.fasta || { echo "Missing VCF2FASTA fasta file (lineage mode with vcf input)!"; exit 1; }
 test -s $output/test_data.input.pangolin.csv || { echo "Missing pangolin output file (lineage mode with vcf input)!"; exit 1; }
-assert_eq $(wc -l $output/test_data.input.pangolin.csv) 2 "Wrong number of pangolin results"
+assert_eq $(wc -l $output/test_data.input.pangolin.csv | cut -d ' ' -f1) 2 "Wrong number of pangolin results"
 
 
 
--- tests/scripts/test_16.sh
+++ tests/scripts/test_16.sh
@@ -10,7 +10,7 @@ output=tests/output/test15
 nextflow main.nf -profile conda --name test_data \
 	--output $output \
 	--fasta $(pwd)/tests/test_data/test_data.fasta \
-    --lineage_mode
+    	--lineage_mode
 
 test -s $output/test_data.assembly.pangolin.csv || { echo "Missing pangolin output file (lineage mode with vcf input)!"; exit 1; }
-assert_eq $(wc -l $output/test_data.assembly.pangolin.csv) 2 "Wrong number of pangolin results"
+assert_eq $(wc -l $output/test_data.assembly.pangolin.csv | cut -d ' ' -f1) 2 "Wrong number of pangolin results"
